e5c184cb4c7682142027fdd09e9ddc26fedf91cd
sironekotoro <develop@sironekotoro.com>
2019-06-22 16:49:41 +0900

    コントローラから受け取った情報をビューで表示する

--- mojo_bbs.pl
+++ mojo_bbs.pl
@@ -3,8 +3,8 @@ use Mojolicious::Lite;
 
 get '/' => sub {
   my $c = shift;
-  my $entry = $c->param('body') ; # クエリストリング body を受け取り $entry に代入
-  $c->stash( kakikomi => $entry );# 追加
+  my $entry = $c->param('body') ;
+  $c->stash( kakikomi => $entry );
   $c->render('index');
 };
 
@@ -15,11 +15,12 @@ __DATA__
 % layout 'default';
 % title 'BBS';
 <h1>掲示板です</h1>
-<!-- ここから4行追加 -->
+
 <form action="/" method="get">
     <input name="body" type="text">
     <input type="submit" value="投稿する">
 </form>
+<p><%= $kakikomi %></p> <!-- 追加 -->
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
